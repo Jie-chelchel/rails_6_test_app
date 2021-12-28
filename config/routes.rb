c09abc8aa198e89e0dba70656b4ebaad708f68b6
Jie Zhang <zhangjiew@gmail.com>
2021-12-27 23:54:30 -0700

    add users create action

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :posts
   root "pages#home"
   get 'about', to: "pages#about"
-
+  get 'signup', to: "users#new"
+  resources :users, except: [:new]
 end
