54051c536d2b4292d19c0ec58ed8eb242a508c17
Jie Zhang <zhangjiew@gmail.com>
2021-12-31 15:14:49 -0700

    delete user and all associated posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   before_save {self.email = email.downcase}
-  has_many :posts
+  has_many :posts, dependent: :destroy
   VALID_EMAIL_REGEX= /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i  
   validates :username, presence: true, length: { in: 3..25 }, uniqueness: {case_sensitive: false}
   validates :email, presence: true, length: { maximum: 105 }, uniqueness: {case_sensitive: false},format:{with:VALID_EMAIL_REGEX, :multiline => true}
